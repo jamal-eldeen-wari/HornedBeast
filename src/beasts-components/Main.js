73d5ab8d26b0cf5e54335d9c16389b14e5a7cb6b
Jamal Eldeen Wari <jamalwari2@gmail.com>
2021-07-28 22:11:13 +0300

    adding lab 4

--- src/beasts-components/Main.js
+++ src/beasts-components/Main.js
@@ -1,31 +1,48 @@
 import React from 'react';
+import BeastForm from './BeastForm';
 import HornedBeast from './HornedBeast';
 
-
-class Main extends React.Component{
-  render(){
-    return(
-
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //Default value
+      hornFilter: -1,
+    };
+  }
+  // this function is responsible for changing the filter based on user input.
+  changeFilter = (changeFilter) => {
+    this.setState({
+      changeFilter: changeFilter,
+    });
+  };
+  render() {
+    return (
       <main>
-        {this.props.hornData.map(beast =>
-            
-          <HornedBeast
-            title = {beast.title}
-            image_url ={beast.image_url}
-            description ={beast.description}
-            keyword = {beast.keyword}
-            horns = {beast.horns}
-            hornFunction = {this.props.hornedBeastsFav}
-            favBeast = {this.props.favBeast}
-            updatingShow = {this.props.updatingShow}
-            dataModeling = {this.props.dataModeling}
-            
-          />
-        )}
-        {/* {console.log('from main',this.props)} */}
+        <BeastForm changeFilter={this.changeFilter} />
 
+        {this.props.hornData
+          .filter((element) =>
+            this.state.changeFilter === -1
+              ? true
+              : element.horn === this.state.hornFilter
+          )
+          .map((beast) => {
+            return (
+              <HornedBeast
+                title={beast.title}
+                image_url={beast.image_url}
+                description={beast.description}
+                keyword={beast.keyword}
+                horns={beast.horns}
+                hornFunction={this.props.hornedBeastsFav}
+                favBeast={this.props.favBeast}
+                updatingShow={this.props.updatingShow}
+                dataModeling={this.props.dataModeling}
+              />
+            );
+          })}
       </main>
-
     );
   }
 }
